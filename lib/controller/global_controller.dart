9e6d456c3ebbf878f7b31e60fe8b2aae81ac0050
samarpan sarkar <samarpansarkar209@gmail.com>
2023-06-11 19:32:29 +0530

    base code

---
+++ lib/controller/global_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController{
+  //Create various variables
+  final RxBool _isLoading = true.obs;
+
+  final RxDouble _lattitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+
+
+}
